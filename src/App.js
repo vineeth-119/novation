25227e51c79cbd2f29d8affd4fd32eba56f98e15
Srikanth Gunturu <sgunturu@modeln.com>
2022-12-12 15:21:03 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { commands } from './utils/commands';
+import React,{useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App =() => {
+
+  const [commandsToRun,setCommandsToRun]=useState([]);
+
+  
+  const handleChange=(e)=>{
+    const value=e.target.value;
+    const checked=e.target.checked;
+    if(checked){
+      setCommandsToRun([
+        ...commandsToRun,value
+      ])
+    }
+    else{
+        setCommandsToRun(commandsToRun.filter((e) =>(e!==value) ));
+    }
+    
+  }
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log(commandsToRun);
+  }
+
+  return(
+    <div className='commands'>
+      
+      <h1>Commands</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <ul>
+        {
+          commands.map((c,i)=>{
+            return(
+                <li>
+                  <input key={i} type="checkbox" name={c.name} value={c.value} id={c.id} onChange={handleChange}  />
+                  <label>{c.name}</label>
+                </li>
+            );
+          })
+        }
+      
+          
+        </ul>
+      
+       <button type="submit">Submit</button>
+      </form>
+    
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/commands.js
@@ -0,0 +1,12 @@
+export const commands=[
+    {
+        id:1,
+        name:"Build",
+        value:"build"
+    },
+    {
+        id:2,
+        name:"DB Migrate",
+        value:"dbmigrate"
+    }
+];
